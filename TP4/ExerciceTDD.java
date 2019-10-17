6e4d0b49761620144779e1241668941bfd8aabcf
louisBrochet <louisbrochet@outlook.com>
2019-10-17 13:19:16 +0200

    Fin TP4

--- TP4/ExerciceTDD.java
+++ TP4/ExerciceTDD.java
@@ -9,15 +9,50 @@ package TP4;
  */
 public class ExerciceTDD {
 	
+	/**
+	 * Fonction qui permet de savoir si le nombre en paramètre est pair ou non
+	 * @param nombre un entier quelconque
+	 * @return vrai ou faux en focntion de s'il est pair
+	 */
 	public boolean estPair(int nombre){
+		if (nombre % 2 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
+	/**
+	 * Fonction qui permet de calculer le périmètre d'un carré
+	 * @param cote un double qui doit être supérieur à 0
+	 * @return retourne le périmètre avec un double. Renvoie 0.0 si cote <= 0
+	 */
 	public double perimetreCarre(double cote) {
-		
+		double res;
+		if(cote <= 0) {
+			res = 0;
+		}
+		else {
+			res = cote * 4;
+		}
+		return res;
 	}
 	
+	/**
+	 * Fonction qui permet de savoir le nombre d'occurence d'un caractère dans une chaîne de caractère 
+	 * @param caractere un caractère quelconque
+	 * @param chaine une chaîne de caractère quelconque
+	 * @return un entier qui correspond au nombre d'occurence de la lettre entrée en paramètre dans la chaîne également en paramètre
+	 */
 	public int contientCaractere(char caractere, String chaine) {
-		
+		int compteur = 0;
+		for (int i = 0; i < chaine.length(); i++) {
+			if(chaine.charAt(i) == caractere) {
+				compteur ++;
+			}
+		}
+		return compteur;
 	}
 	
 	/**
--- TP4/ExerciceTDDTest.java
+++ TP4/ExerciceTDDTest.java
@@ -17,7 +17,18 @@ class ExerciceTDDTest {
 	void test() {
 		ExerciceTDD exo = new ExerciceTDD();
 		assertEquals(true, exo.estPair(0));
+		assertEquals(true, exo.estPair(-2));
+		assertEquals(false, exo.estPair(1));
 		assertEquals(33.2, exo.perimetreCarre(8.3));
+		assertEquals(0.0, exo.perimetreCarre(-9.0));
+		assertEquals(0.0, exo.perimetreCarre(0.0));
+		assertEquals(20, exo.perimetreCarre(5));
+		assertEquals(3, exo.contientCaractere('a', "zaebaebzaz"));
+		assertEquals(0, exo.contientCaractere('s', "zaebaebzaz"));
+		assertEquals(0, exo.contientCaractere('à', "zaebaebzaz"));
+		assertEquals(3, exo.contientCaractere('z', "zaeb    aebzaz"));
+		
+		
 	}
 
 }
